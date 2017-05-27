e1a2a147d5d514221bcf151ecce1dfc40e3bfc76
Dmitry Frank <mail@dmitryfrank.com>
2017-05-27 13:55:04 +0100

    Implement autogenerated deps init
    PUBLISHED_FROM=36f2153f90a61bc71c28451ad3bfbd75fd06219b

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
+#include <stdio.h>
 #include "fw/src/mgos_app.h"
 
-#include "blynk.h"
-#include "mos_mjs.h"
-
 enum mgos_app_init_result mgos_app_init(void) {
-  blynk_init();
-  mjs_init();
   return MGOS_APP_INIT_SUCCESS;
 }
