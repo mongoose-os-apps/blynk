1d6414598366efd0d99ccb321e9a2a6aa4fc5189
Dmitry Frank <mail@dmitryfrank.com>
2017-08-08 14:42:28 +0300

    Add id arg to the blynk callback
    PUBLISHED_FROM=3f1bbcdc34385c99b0c426acca11e738f8994cb0

--- fs/init.js
+++ fs/init.js
@@ -2,15 +2,15 @@ load('api_gpio.js');
 load('api_blynk.js');
 load('api_sys.js');
 
-Blynk.setHandler(function(conn, cmd, pin, val) {
+Blynk.setHandler(function(conn, cmd, id, pin, val) {
   let ram = Sys.free_ram() / 1024;
   if (cmd === 'vr') {
   	// When reading any virtual pin, report free RAM in KB
-    Blynk.virtualWrite(conn, pin, ram);
+    Blynk.virtualWrite(conn, pin, ram, id);
   } else if (cmd === 'vw') {
   	// Writing to virtual pin translate to writing to physical pin
     GPIO.set_mode(pin, GPIO.MODE_OUTPUT);
     GPIO.write(pin, val);
   }
-  print('BLYNK JS handler, ram', ram, cmd, pin, val);
+  print('BLYNK JS handler, ram', ram, cmd, id, pin, val);
 }, null);
